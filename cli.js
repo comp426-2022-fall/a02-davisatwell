1bc651534ccee3eadd20b1565a6784fd359eb73c
Davis Atwell <77867726+davisatwell@users.noreply.github.com>
2022-10-30 17:07:12 -0400

    a02v1.0.1

--- cli.js
+++ cli.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-
-// Import Dependencies
-import minimist from 'minimist'; //minimist is the parser on any args on the command line
+import minimist from 'minimist';
 import moment from 'moment-timezone';
-import fetch from 'node-fetch';
+import node_fetch from 'node-fetch';
 
-//Load args into a const.
 const args = minimist(process.argv.slice(2));
 
-// default
-if (args.h) {
-    try {console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+let timezone = moment.tz.guess();
+
+if(args.t){
+    timezone = args.t;
+}
+
+if(args.h){
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
@@ -18,61 +20,45 @@ if (args.h) {
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
     -j            Echo pretty JSON from open-meteo API and exit.
     `)
-    process.exit(0);
+}else{
+    let latitude = '35';
+    if(args.n){
+        latitude = args.n;
     }
-    catch (err) {
-        process.exit(1);
+
+    if(args.s){
+        latitude = args.s;
+    }else{
+        console.log("Latitude must be in range");
     }
-}
-let timezone = moment.tz.guess()
-if (args.z) {
-    timezone = args.z;
-}
 
-let latitude = '35.92';
-if (args.n) {
-    latitude = args.n;
-}
-if (args.s) {
-    latitude = '-' + args.s;
-}
+    let longtitude = '79';
+    if(args.e){
+        longtitude = args.e;
+    }
 
-let longitude = '-79.05';
-if (args.e) {
-    longtitude = args.e;
-}
-if (args.w) {
-    longitude = '-' + args.w;
-}
-const base_url = 'https://api.open-meteo.com/v1/forecast'
-const data_string = 'latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,windspeed_180m,winddirection_180m&daily=weathercode,precipitation_hours&temperature_unit=fahrenheit&timezone=' + timezone
-const url = base_url + '?' + data_string;
+    if(args.w){
+            longtitude = args.w;
+    }else{
+        console.log("Longitude must be in range")
+    }
 
-const response = await fetch(url);
-const data = await response.json();
+    const response = await node_fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longtitude + '&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,precipitation_sum&timezone=' + timezone)
 
-if (args.j) {
-    console.log(data);
-    process.exit(0);
-}
+    const data = await response.json();
 
-let days;
-if (args.d != null) {
-    days = args.d;
-} else {
-    days = 1;
-}
+    if(args.j){
+        console.log(data);
+        process.exit(0);
+    }
 
-if (data.daily.precipitation_hours[days] != 0.0) {
-    console.log("You might need your galoshes");
-} else{
-    console.log("You will not need your galoshes");
-}
+    const days = args.d;
 
-if (days == 0) {
-    console.log("today.")
-} else if (days > 1) {
-    console.log("in " + days + " days.")
-} else {
-    console.log("tomorrow.")
+    if (days == 0) {
+        console.log("today.")
+    } else if (days > 1) {
+        console.log("in " + days + " days.")
+    } else {
+        console.log("tomorrow.")
+    }
 }
